fffbb2501c09dfa65a13eab6baaefe8c6bff982a
510208 <xux510208@gmail.com>
2024-01-25 17:07:34 +0800

    完成基本框架

---
+++ servdown/servdown/MinecraftHelper.vb
@@ -0,0 +1,53 @@
+﻿Imports System.Diagnostics
+
+Module MinecraftHelper
+    Private pythonScriptPath As String = "get_file.py"
+
+    ' 調用 Python 腳本中的 get_download_link 函數
+    Function GetDownloadLink(mcVersion As String) As String
+        Dim processStartInfo As New ProcessStartInfo With {
+            .FileName = "python",
+            .Arguments = $"{pythonScriptPath} --version {mcVersion}",
+            .RedirectStandardOutput = True,
+            .UseShellExecute = False,
+            .CreateNoWindow = True
+        }
+
+        Using process As New Process() With {.StartInfo = processStartInfo}
+            process.Start()
+            Return process.StandardOutput.ReadToEnd()
+        End Using
+    End Function
+
+    ' 調用 Python 腳本中的 download_server 函數
+    Sub DownloadServer(mcVersion As String)
+        Dim processStartInfo As New ProcessStartInfo With {
+            .FileName = "python",
+            .Arguments = $"{pythonScriptPath} --version {mcVersion} -d",
+            .RedirectStandardOutput = True,
+            .UseShellExecute = False,
+            .CreateNoWindow = True
+        }
+
+        Using process As New Process() With {.StartInfo = processStartInfo}
+            process.Start()
+            Console.WriteLine(process.StandardOutput.ReadToEnd())
+        End Using
+    End Sub
+
+    ' 調用 Python 腳本中的 get_all_versions 函數
+    Function GetAllVersions() As String
+        Dim processStartInfo As New ProcessStartInfo With {
+            .FileName = "python",
+            .Arguments = $"{pythonScriptPath} --all_ver",
+            .RedirectStandardOutput = True,
+            .UseShellExecute = False,
+            .CreateNoWindow = True
+        }
+
+        Using process As New Process() With {.StartInfo = processStartInfo}
+            process.Start()
+            Return process.StandardOutput.ReadToEnd()
+        End Using
+    End Function
+End Module
---
+++ servdown/servdown/check_enviroment.vb
@@ -0,0 +1,48 @@
+﻿Imports System.IO
+
+Module check_enviroment
+    Function chkJava() As Boolean
+        ' Dim openFolder As New FolderBrowserDialog()
+
+        ' 顯示對話框
+        ' Dim result As DialogResult = openFolder.ShowDialog()
+
+        ' 檢查使用者是否按下了 "確定" 按鈕
+        Dim javaPath As Boolean = GetJavaPath()
+
+        ' 檢查 java.exe 是否存在
+        If javaPath Then
+            Return True
+        Else
+            Return False
+        End If
+        ' 使用者取消了選擇資料夾
+        Return False
+    End Function
+
+    Function GetJavaPath() As String
+        Dim paths As String() = Environment.GetEnvironmentVariable("PATH").Split(Path.PathSeparator)
+
+        For Each path In paths
+            Dim javaExePath As String = path + "\java.exe"
+
+            If path.Contains("java") AndAlso File.Exists(javaExePath) Then
+                ' 找到包含 "java" 且有 java.exe 的路徑
+                Return True
+            End If
+        Next
+
+        Return False
+    End Function
+
+    ' 檢查空閒記憶體是否超過3GB
+    Function chkMemory() As Boolean
+        Dim freeMemory As Double = My.Computer.Info.AvailablePhysicalMemory / 1024 / 1024 / 1024 ' 轉換為GB
+        Return freeMemory > 3
+    End Function
+
+    ' 檢查網際網路連線是否正常
+    Function chkNet() As Boolean
+        Return My.Computer.Network.IsAvailable
+    End Function
+End Module
